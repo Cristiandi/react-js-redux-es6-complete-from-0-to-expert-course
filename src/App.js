8a7aa7b3c59d5d8437af3f3ec90493396f19a94a
Cristian David Ippolito Carrillo <cristiandavidippolito@gmail.com>
2019-04-28 18:19:31 -0500

    feat(course): using react-flexbox-grid

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Grid, Col, Row } from 'react-flexbox-grid'
 import LocationList from './components/LocationList';
+import './App.css';
 
 const cities = [
   'Cali, CO',
@@ -20,9 +21,19 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
-        <LocationList cities={cities} onSelectedLocation={this.handleSelectedLocation} />
-      </div>
+      <Grid>
+        <Row>
+          Titulo
+        </Row>
+        <Row>
+          <Col xs={12} md={6}>
+            <LocationList cities={cities} onSelectedLocation={this.handleSelectedLocation} />
+          </Col>
+          <Col xs={12} md={6}>
+            <div className={'details'} ></div>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
