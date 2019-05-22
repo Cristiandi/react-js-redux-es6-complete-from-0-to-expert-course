508613bfe5a16a0435839ab640d745a573afbabe
Cristian David Ippolito Carrillo <cristiandavidippolito@gmail.com>
2019-05-22 07:19:38 -0500

    feat(course): better forecast extended

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -21,7 +21,18 @@ class ForecastExtended extends Component {
   }
   
   componentDidMount() {
-    const url_forecast = `${url_base_weather}forecast?q=${this.props.city}&appid=${api_key}`;
+    this.updateCity(this.props.city);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.city !== this.props.city) {
+      this.setState({ forecastData: null });
+      this.updateCity(nextProps.city);
+    }
+  }
+
+  updateCity = city => {
+    const url_forecast = `${url_base_weather}forecast?q=${city}&appid=${api_key}`;
 
     fetch(url_forecast)
       .then(data => data.json())
