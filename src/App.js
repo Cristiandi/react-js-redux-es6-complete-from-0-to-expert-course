b8b36b4ff6119a1d284fd9d0a99d42c42834a570
Cristian David Ippolito Carrillo <cristiandavidippolito@gmail.com>
2019-06-05 18:25:58 -0500

    feat(course): adding redux and creating the store

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import {
   AppBar,
   Typography,
   Toolbar } from '@material-ui/core';
-import { Grid, Col, Row } from 'react-flexbox-grid'
+import { Grid, Col, Row } from 'react-flexbox-grid';
+import { createStore } from 'redux';
 import LocationList from './components/LocationList';
 import ForecastExtended from './components/ForecastExtended';
 import './App.css';
@@ -18,6 +19,8 @@ const cities = [
   'Popayan, CO',
   'Pasto, CO'
 ];
+
+const store = createStore(() => {}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 class App extends Component {
   constructor () {
@@ -34,6 +37,8 @@ class App extends Component {
     console.log('====================================');
     console.log('handleSelectionLocation', city);
     console.log('====================================');
+    const action = { type: 'setCity', value: city };
+    store.dispatch(action)
   }
   render() {
     const { city } = this.state;
