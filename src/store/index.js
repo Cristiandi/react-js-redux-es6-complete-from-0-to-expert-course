a3e228042961e0c45deef8a8277b702f27aaedf6
Cristian David Ippolito Carrillo <cristiandavidippolito@gmail.com>
2019-07-08 21:53:07 -0500

    feat(course): improment into the global state

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { city } from '../reducers/city';
+import reducers from '../reducers';
 
 const initialState = {
   city: 'Cali, CO'
@@ -10,7 +10,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 
 export const store = createStore(
-  city,
+  reducers,
   initialState,
   composeEnhancers(applyMiddleware(thunk))
 );
